1ac7d1cc3e9086f1bfc45e4f26234871d2f5d123
Doug MacEwen <doug@glass-imaging.com>
2025-05-20 14:53:06 -0700

    Add neogit plugin and tie it to keymap <leader>g

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -141,6 +141,20 @@ return {
             })
         end
     },
+    {
+        "NeogitOrg/neogit",
+        lazy = false,
+        dependencies = {
+            "nvim-lua/plenary.nvim",         -- required
+            "sindrets/diffview.nvim",        -- optional - Diff integration
+
+            -- Only one of these is needed.
+            "nvim-telescope/telescope.nvim", -- optional
+        },
+        config = function()
+            vim.keymap.set("n", "<leader>g", "<cmd>Neogit<CR>", { desc = "Open Neogit" })
+        end,
+    },
     -- Misc Tools
     {
         'zbirenbaum/copilot.lua',
